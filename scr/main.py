ea350dec9c57cf23ef0266b5d9b1ef28a315ec7a
Abc Bolinhas <abc@bolinhas.com>
2024-04-22 18:52:27 -0300

    Alterado arquivo main!

--- scr/main.py
+++ scr/main.py
@@ -3,6 +3,23 @@ from settings import HOST, PORT, DEBUG
 
 app = Flask(__name__)
 
+''' chamadas dos formulários '''
+@app.route('/')
+def formIndex():
+    return "<h1>Rota da página inicial da nossa WEB APP</h1>", 200
+
+@app.route('/funcionario/')
+def formListaFuncionario():
+    return "<h1>Rota da página de Funcionários da nossa WEB APP</h1>", 200
+
+@app.route('/cliente/')
+def formListaCliente():
+    return "<h1>Rota da página de Clientes da nossa WEB APP</h1>", 200
+
+@app.route('/produto/')
+def formListaProduto():
+    return "<h1>Rota da página de Produtos da nossa WEB APP</h1>", 200
+
 if __name__ == "__main__":
     """ Inicia o aplicativo WEB Flask """
     app.run(host=HOST, port=PORT, debug=DEBUG)
