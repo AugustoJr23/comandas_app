df4a43e37129e4d052b510266c0118a5c86296e2
Abc Bolinhas <abc@bolinhas.com>
2024-05-12 16:33:14 -0300

    Alterado cliente.py e produto.py

--- scr/mod_cliente/cliente.py
+++ scr/mod_cliente/cliente.py
@@ -1,12 +1,28 @@
 from flask import Blueprint, render_template
+import requests
+#from mod_login.login import validaToken
+from settings import getHeadersAPI, ENDPOINT_CLIENTE
 
 bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
 
-''' rotas dos formulários '''
-@bp_cliente.route('/')
-def formListaCliente():
-    return render_template('formListaCliente.html'), 200
+''' rotas dos clientes '''
 
 @bp_cliente.route('/formCliente/', methods=['GET'])
 def formCliente():
     return render_template('formCliente.html')
+
+@bp_cliente.route('/', methods=['GET', 'POST'])
+def formListaCliente():
+    try:
+        response = requests.get(ENDPOINT_CLIENTE, headers=getHeadersAPI())
+        result = response.json()
+
+        print(result) # para teste
+        print(response.status_code) # para teste
+        if (response.status_code != 200):
+
+            raise Exception(result)
+        
+        return render_template('formListaCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
--- scr/mod_produto/produto.py
+++ scr/mod_produto/produto.py
@@ -1,12 +1,27 @@
 from flask import Blueprint, render_template
+import requests
+#from mod_login.login import validaToken
+from settings import getHeadersAPI, ENDPOINT_PRODUTO
 
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
 
 ''' rotas dos produtos '''
-@bp_produto.route('/')
-def formListaProduto():
-    return render_template('formListaProduto.html'), 200
-
 @bp_produto.route('/formProduto/', methods=['GET'])
 def formProduto():
     return render_template('formProduto.html')
+
+@bp_produto.route('/', methods=['GET', 'POST'])
+def formListaProduto():
+    try:
+        response = requests.get(ENDPOINT_PRODUTO, headers=getHeadersAPI())
+        result = response.json()
+
+        print(result) # para teste
+        print(response.status_code) # para teste
+        if (response.status_code != 200):
+
+            raise Exception(result)
+        
+        return render_template('formListaProduto.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])
