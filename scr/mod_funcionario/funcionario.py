57e3e5c7b8dd339da195ecc0c7055ad74c171f80
Abc Bolinhas <abc@bolinhas.com>
2024-05-14 21:45:19 -0300

    Criado funcoes.py, criado uma parte de edição de funcionarios

--- scr/mod_funcionario/funcionario.py
+++ scr/mod_funcionario/funcionario.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 import requests
+from funcoes import Funcoes
 #from mod_login.login import validaToken
 from settings import getHeadersAPI, ENDPOINT_FUNCIONARIO
 
@@ -24,5 +25,53 @@ def formListaFuncionario():
             raise Exception(result)
         
         return render_template('formListaFuncionario.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+    
+@bp_funcionario.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_funcionario = request.form['id']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = Funcoes.get_password_hash(request.form['senha'])
+
+        # monta o JSON para envio a API
+        payload = {'id_funcionario': id_funcionario, 'nome': nome, 'matricula': matricula, 'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
+
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(ENDPOINT_FUNCIONARIO, headers=getHeadersAPI(), json=payload)
+        result = response.json()
+
+        print(result) # [{'msg': 'Cadastrado com sucesso!', 'id': 13}, 200]
+        print(response.status_code) # 200
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result)
+
+        return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+
+@bp_funcionario.route("/form-edit-funcionario", methods=['POST'])
+def formEditFuncionario():
+    try:
+        # ID enviado via FORM
+        id_funcionario = request.form['id']
+        
+        # executa o verbo GET da API buscando somente o funcionário selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_FUNCIONARIO + id_funcionario, headers=getHeadersAPI())
+        result = response.json()
+
+        if (response.status_code != 200):
+            raise Exception(result)
+        
+        # renderiza o form passando os dados retornados
+        return render_template('formFuncionario.html', result=result[0])
     except Exception as e:
         return render_template('formListaFuncionario.html', msgErro=e.args[0])
