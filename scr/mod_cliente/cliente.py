baf172cf8d6e45bd4ed0f05b3aa4b5f7ea3ad772
Abc Bolinhas <abc@bolinhas.com>
2024-05-28 06:19:26 -0300

    Criado o CRUD de Clientes!

--- scr/mod_cliente/cliente.py
+++ scr/mod_cliente/cliente.py
@@ -9,10 +9,12 @@ bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_fold
 ''' rotas dos clientes '''
 
 @bp_cliente.route('/formCliente/', methods=['GET'])
+@validaToken
 def formCliente():
     return render_template('formCliente.html')
 
 @bp_cliente.route('/', methods=['GET', 'POST'])
+@validaToken
 def formListaCliente():
     try:
         response = requests.get(ENDPOINT_CLIENTE, headers=getHeadersAPI())
@@ -32,7 +34,7 @@ def formListaCliente():
 def insert():
     try:
         # dados enviados via FORM
-        id_funcionario = request.form['id']
+        id_cliente = request.form['id']
         nome = request.form['nome']
         cpf = request.form['cpf']
         logradouro = request.form['logradouro']
@@ -44,7 +46,7 @@ def insert():
         cep = request.form['cep']
 
         # monta o JSON para envio a API
-        payload = {'id_funcionario': id_funcionario, 'nome': nome, 'cpf': cpf, 'logradouro': logradouro, 'numero': numero, 'complemento': complemento, ' bairro':   bairro, 'cidade': cidade, 'estado': estado, 'cep': cep}
+        payload = {'id_cliente': id_cliente, 'nome': nome, 'cpf': cpf, 'logradouro': logradouro, 'numero': numero, 'complemento': complemento, 'bairro':   bairro, 'cidade': cidade, 'estado': estado, 'cep': cep}
         
         # executa o verbo POST da API e armazena seu retorno
         response = requests.post(ENDPOINT_CLIENTE, headers=getHeadersAPI(), json=payload)
@@ -56,6 +58,76 @@ def insert():
         if (response.status_code != 200 or result[1] != 200):
             raise Exception(result)
 
-        return redirect(url_for('funcionario.formListaCliente', msg=result[0]))
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
     except Exception as e:
         return render_template('formListaCliente.html', msgErro=e.args[0])
+    
+@bp_cliente.route("/formEditCliente", methods=['POST'])
+def formEditCliente():
+    try:
+        # ID enviado via FORM
+        id_cliente = request.form['id']
+
+        # executa o verbo GET da API buscando somente o funcionário selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_CLIENTE + id_cliente, headers=getHeadersAPI())
+        result = response.json()
+
+        if (response.status_code != 200):
+            raise Exception(result)
+        
+        # renderiza o form passando os dados retornados
+        return render_template('formCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+
+@bp_cliente.route('/edit', methods=['POST'])
+def edit():
+    try:
+
+        # dados enviados via FORM
+        id_cliente = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        logradouro = request.form['logradouro']
+        numero = request.form['numero']
+        complemento = request.form['complemento']
+        bairro = request.form['bairro']
+        cidade = request.form['cidade']
+        estado = request.form['estado']
+        cep = request.form['cep']
+
+        # monta o JSON para envio a API
+        payload = {'id_cliente': id_cliente, 'nome': nome, 'cpf': cpf, 'logradouro': logradouro, 'numero': numero, 'complemento': complemento, 'bairro':   bairro, 'cidade': cidade, 'estado': estado, 'cep': cep}
+        
+        # executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(ENDPOINT_CLIENTE + id_cliente, headers=getHeadersAPI(), json=payload)
+        result = response.json()
+
+        print(result[0])
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result)
+        
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
+    
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+    
+@bp_cliente.route('/delete', methods=['POST'])
+def delete():
+    try:
+        # dados enviados via FORM
+        id_cliente = request.form['id']
+
+        # executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(ENDPOINT_CLIENTE + id_cliente, headers=getHeadersAPI())
+        result = response.json()
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result)
+    
+        return jsonify(erro=False, msg=result[0])
+    
+    except Exception as e:
+        return jsonify(erro=True, msgErro=e.args[0])
